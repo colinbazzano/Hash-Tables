a27797be1b0a7570f97656cd555167fa3fbb672c
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-05-10 11:04:12 -0400

    adds no dups solution

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,12 +1,28 @@
-cache = {}
+# cache = {}
 
+
+# def no_dups(s):
+#     for word in s.split(" "):
+#         if word not in cache:
+#             cache[word] = word
+
+#         return cache[word]
+
+# Below is my unfinished version, below is solution
+# documented by me
 
 def no_dups(s):
-    for word in s.split(" "):
-        if word not in cache:
-            cache[word] = word
+    words = s.split()  # split the words, it is a list
 
-        return cache[word]
+    seen = {}  # words we have seen
+    result = []  # return to show the words without duplicates
+
+    for w in words:  # for each word in the list
+        if w not in seen:  # if we haven't seen it yet
+            result.append(w)  # add it to the result
+            seen[w] = True  # set this to true so we won't append it again
+    # returns them as a string rather a list with each string in it
+    return " ".join(result)
 
 
 if __name__ == "__main__":

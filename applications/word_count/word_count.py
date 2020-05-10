ca686d6c6c5a9c2ed53cbd38cb1e63727a078953
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-05-10 11:16:44 -0400

    adds word count solution

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,22 +1,40 @@
 
 
+# def word_count(s):
+#     words = {}
+#     # split the words at a space line break
+#     # words = [word for word in s.split(" ")]
+#     # print("words", words)
+#     if s == "":
+#         return words
+#     else:
+#         for word in s.split(" "):
+#             if word not in words:
+#                 words[word] = 1
+#             else:
+#                 words[word] += 1
+#         return words
+# ignore special characters
+# make it lowercase
+# loop through and add count words
+
+# above is me, below is solution documented by me
+
 def word_count(s):
-    words = {}
-    # split the words at a space line break
-    # words = [word for word in s.split(" ")]
-    # print("words", words)
-    if s == "":
-        return words
-    else:
-        for word in s.split(" "):
-            if word not in words:
-                words[word] = 1
-            else:
-                words[word] += 1
-        return words
-    # ignore special characters
-    # make it lowercase
-    # loop through and add count words
+    # make a tranformation of the letters we dont want
+    tr = str.maketrans("", "", '":;,.-+=/\\|[]{}()*^&')
+    # make everything lower case
+    s = s.translate(tr).lower()
+
+    words = s.split()  # split the words
+    counts = {}  # store the count of each word
+
+    for w in words:
+        if w not in counts:
+            counts[w] = 1  # if it's not there, set the count to 1
+        else:  # it IS there
+            counts[w] += 1  # add 1 to the count
+    return counts
 
 
 if __name__ == "__main__":
